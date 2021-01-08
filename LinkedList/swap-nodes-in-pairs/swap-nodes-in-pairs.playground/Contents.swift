f77ef4ef2aceeef2cc448e623cb3bf1de0e09583
GuitarPlayer-Ma <138910152@qq.com>
2021-01-08 16:39:06 +0800

    两两交换链表中的节点

--- LinkedList/swap-nodes-in-pairs/swap-nodes-in-pairs.playground/Contents.swift
+++ LinkedList/swap-nodes-in-pairs/swap-nodes-in-pairs.playground/Contents.swift
@@ -13,9 +13,9 @@ func swapPairs(_ head: ListNode?) -> ListNode? {
     if head == nil || head?.next == nil {
         return head
     }
-    let ret = head?.next
-    head?.next = swapPairs(head?.next?.next)
-    ret?.next = head
+    let ret = head?.next // 保存head.next节点
+    head?.next = swapPairs(head?.next?.next) // head的next指针指向翻转后的链表
+    ret?.next = head // head.next节点的next指针指向原head节点（局部翻转）
     return ret
 }
 
