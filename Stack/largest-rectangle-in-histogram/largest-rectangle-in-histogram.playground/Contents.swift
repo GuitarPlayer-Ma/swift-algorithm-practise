8e3c378c579bfb8c22fde95d6bff6533e762f18d
GuitarPlayer-Ma <138910152@qq.com>
2021-01-17 20:55:25 +0800

    柱状图中的最大矩形

--- Stack/largest-rectangle-in-histogram/largest-rectangle-in-histogram.playground/Contents.swift
+++ Stack/largest-rectangle-in-histogram/largest-rectangle-in-histogram.playground/Contents.swift
@@ -14,10 +14,22 @@ func largestRectangleArea(_ heights: [Int]) -> Int {
 }
 
 // 单调栈
-//func largestRectangleArea1(_ heights: [Int]) -> Int {
-//    var result = 0
-//
-//    return result
-//}
+func largestRectangleArea1(_ heights: [Int]) -> Int {
+    var area = 0
+    var stack: [Int] = []
+    
+    // 在数组末尾追加Int.min，通常在单调栈算法中称之为哨兵，其作用
+    // 在于保证栈中所有元素都能够出栈，确保所有元素得到计算
+    // 本题中为了保证栈中所有元素都能出栈，追加Int.min
+    let heightPlus = heights + [Int.min]
+    for i in heightPlus.indices {
+        while !stack.isEmpty && heightPlus[stack.last!] > heightPlus[i] {
+            let height = heightPlus[stack.removeLast()]
+            area = max(area, height * (i - (stack.last ?? -1) - 1))
+        }
+        stack.append(i)
+    }
+    return area
+}
 
-largestRectangleArea([2,1,5,6,2,3])
+largestRectangleArea1([2,1,5,6,2,3])
